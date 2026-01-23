2b597ff15888c3377cbf457a4fba6cf012b91681
Vaishnavi Gopi <vaishnavigopi1717@gmail.com>
2026-01-23 12:15:03 +0530

    Add files via upload

---
+++ bubble.py
@@ -0,0 +1,8 @@
+n=int(input("Enter number of elements:"))
+arr=list(map(int,input("Enter elements:").split()))
+for i in range(n-1):
+    for j in range (n-i-1):
+        if arr[j]>arr[j+1]:
+            arr [j], arr[j+1] = arr[j+1], arr[j]
+
+print("Sorted array",arr)            
