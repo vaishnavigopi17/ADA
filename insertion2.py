104bae875801dc74bbdaf1b690fde9c89cf51461
Vaishnavi Gopi <vaishnavigopi1717@gmail.com>
2026-01-30 12:20:10 +0530

    Add files via upload

--- insertion2.py
+++ insertion2.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 def insert_sort(A):
     n=len(A)
     for i in range(1,n):
@@ -7,7 +10,13 @@ def insert_sort(A):
             A[j+1]=A[j]
             j=j-1
         A[j+1]=v
-arr=[2,7,3,9,10]
-insert_sort(arr)
-print(arr)
-            
+n_list=[2000,1000,5000,4000,10000]
+sort_time=[]
+for n in n_list:
+    arr=[random.randint(1,100) for _ in range(n)]
+    s_t=time.time()
+    insert_sort(arr)
+    e_t=time.time()
+    sort_time.append(e_t-s_t)
+print(n)
+print(sort_time)
